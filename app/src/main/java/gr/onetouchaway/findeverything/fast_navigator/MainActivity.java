aedcca26c360f8190da4d4be185a4a030cc38412
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-04 23:53:49 +0300

    MainActivity modifications

--- app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MainActivity.java
+++ app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MainActivity.java
@@ -2,12 +2,35 @@ package gr.onetouchaway.findeverything.fast_navigator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Dustbin;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    Button btnAdd,btnDelete,btnEdit;
+    EditText editUser;
+    Dustbin dustbinSelected = null;
+    List<Dustbin> dustbins = new ArrayList<Dustbin>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnEdit = (Button)findViewById(R.id.btnEdit);
+        btnDelete = (Button)findViewById(R.id.btnDelete);
+        editUser = (EditText) findViewById(R.id.editUsername);
+
     }
+
+
 }
