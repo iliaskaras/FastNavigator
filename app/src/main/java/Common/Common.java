4c2089ef7e36c7e898287822f6e4286fa58ac396
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-04 23:49:58 +0300

    Added Common and HTTPDataHandler classes

---
+++ app/src/main/java/Common/Common.java
@@ -0,0 +1,31 @@
+package Common;
+
+import Model.Dustbin;
+
+/**
+ * Created by ILIAS on 4/7/2018.
+ */
+
+public class Common {
+
+    private static String DB_NAME = "navigation_map";
+    private static String COLLECTION_NAME = "dustbin";
+    public static String API_KEY = "waqGYV92hh5RC-PbUb8VI_TFkXnKxGZH";
+
+
+    public static String getAddressSingle(Dustbin dustbin){
+        String baseUrl = String.format("https://api.mlab.com/api/1/databases/$s/collections/$s",DB_NAME,COLLECTION_NAME);
+        StringBuilder stringBuilder = new StringBuilder(baseUrl);
+        stringBuilder.append("/"+dustbin.get_id().getOid()+"?apiKey="+API_KEY);
+
+        return stringBuilder.toString();
+    }
+
+    public static String getAddressAPI(){
+        String baseUrl = String.format("https://api.mlab.com/api/1/databases/$s/collections/$s",DB_NAME,COLLECTION_NAME);
+        StringBuilder stringBuilder = new StringBuilder(baseUrl);
+        stringBuilder.append("?apiKey="+API_KEY);
+
+        return stringBuilder.toString();
+    }
+}
