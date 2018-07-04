927136a00614692f5c2c5b181532a30d7c11c1b7
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-05 01:53:17 +0300

    Fixes on wrong usage of api

--- app/src/main/java/Common/Common.java
+++ app/src/main/java/Common/Common.java
@@ -14,15 +14,17 @@ public class Common {
 
 
     public static String getAddressSingle(Dustbin dustbin){
-        String baseUrl = String.format("https://api.mlab.com/api/1/databases/$s/collections/$s",DB_NAME,COLLECTION_NAME);
+        String baseUrl = String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME);
         StringBuilder stringBuilder = new StringBuilder(baseUrl);
-        stringBuilder.append("/"+dustbin.get_id().getOid()+"?apiKey="+API_KEY);
+        stringBuilder.append("/"+dustbin.get_id()
+                .getOid()
+                +"?apiKey="+API_KEY);
 
         return stringBuilder.toString();
     }
 
     public static String getAddressAPI(){
-        String baseUrl = String.format("https://api.mlab.com/api/1/databases/$s/collections/$s",DB_NAME,COLLECTION_NAME);
+        String baseUrl = String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME);
         StringBuilder stringBuilder = new StringBuilder(baseUrl);
         stringBuilder.append("?apiKey="+API_KEY);
 
