a47f2e316434fbe351c56d0165387c3c2e73c4c3
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-04 23:57:20 +0300

    Id.java added

---
+++ app/src/main/java/Model/Id.java
@@ -0,0 +1,18 @@
+package Model;
+
+/**
+ * Created by ILIAS on 4/7/2018.
+ */
+
+public class Id {
+
+    private String oid;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+}
