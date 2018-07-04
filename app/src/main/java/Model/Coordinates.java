8517a5138644ebe939732ec9aefecdbdaa876264
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-05 01:57:52 +0300

    Field name changes

---
+++ app/src/main/java/Model/Coordinates.java
@@ -0,0 +1,35 @@
+package Model;
+
+/**
+ * Created by ILIAS on 3/7/2018.
+ */
+
+public class Coordinates {
+
+    private double lat;
+    private double longitude;
+
+    public Coordinates(double latitude, double longitude) {
+        this.lat = latitude;
+        this.longitude = longitude;
+    }
+
+    public Coordinates() {
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
