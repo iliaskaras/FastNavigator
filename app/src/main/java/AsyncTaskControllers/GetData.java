36f7848d8a1ecf33fe4cdce525d2c06a3565ad07
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-08 21:43:01 +0300

    code refactoring

--- app/src/main/java/AsyncTaskControllers/GetData.java
+++ app/src/main/java/AsyncTaskControllers/GetData.java
@@ -6,39 +6,17 @@ package AsyncTaskControllers;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.ListView;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-
-import Adapters.CustomAdapter;
-import Model.Dustbin;
 import Services.GetService;
 
-import static android.content.ContentValues.TAG;
 
 /** Process Data */
 public class GetData extends AsyncTask<String,Void,String> {
 
-    private static final String TAG = "AARestTask";
-    public static final String HTTP_RESPONSE = "httpResponse";
 
     ProgressDialog progressDialog;
 
     public DownloadTaskListener mListener;
-
-//    public interface DownloadTaskListener
-//    {
-//        public void onDownloadFinish(String s);
-//        public void onDownloadProgress(float progress);
-//    }
 
     public GetData(ProgressDialog progressDialog, Context mContext) {
         this.progressDialog = progressDialog;
@@ -59,14 +37,6 @@ public class GetData extends AsyncTask<String,Void,String> {
 
         if(mListener != null)
         {
-//            /** Done Process */
-//            /** Use Gson to parse Json to Class*/
-//            Gson gson = new Gson();
-//            Type listType = new TypeToken<List<Dustbin>>(){}.getType();
-//            dustbins = gson.fromJson(s,listType);
-//            CustomAdapter adapter = new CustomAdapter(mContext.getApplicationContext(),dustbins);
-//            listView.setAdapter(adapter);
-//            progressDialog.dismiss();
             mListener.onDownloadFinish(s);
 
         }
