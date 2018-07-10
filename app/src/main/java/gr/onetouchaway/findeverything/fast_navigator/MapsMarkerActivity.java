d98af4ec8583c95231d2c36f80dcfd633614d79d
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-10 23:49:11 +0300

    added MapsMarkerActivity

---
+++ app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
@@ -0,0 +1,41 @@
+package gr.onetouchaway.findeverything.fast_navigator;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ILIAS on 10/7/2018.
+ */
+
+public class MapsMarkerActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+    // Include the OnCreate() method here too, as described above.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.fragment_map);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
