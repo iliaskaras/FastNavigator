fc1f72a3e812d4e01dcbbbdc65ce860a112d9624
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-11 23:53:53 +0300

    Added File Writer Controller class

--- app/src/main/java/Controllers/FileWriterController.java
+++ app/src/main/java/Controllers/FileWriterController.java
@@ -1,5 +1,10 @@
 package Controllers;
 
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 import Model.Dustbin;
 
@@ -8,15 +13,38 @@ import Model.Dustbin;
  */
 
 public class FileWriterController {
-
+    private Context mContext;
+    final static String fileName = "data.txt";
+    final static String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+//            + "/instinctcoder/readwrite/" ;
 
     public FileWriterController() {
     }
 
-    public void writeJsonToResources(List<Dustbin> dustbinsList){
+    public FileWriterController(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void writeTxtToResources(List<Dustbin> dustbinsList){
 
 
         try{
+            new File(path  ).mkdir();
+            File file = new File(path+ fileName);
+
+//            File file = this.mContext.getFileStreamPath("test.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileOutputStream writer = this.mContext.openFileOutput(file.getName(), Context.MODE_PRIVATE);
+
+            for (Dustbin dustbin: dustbinsList){
+                writer.write(dustbin.getLocation().getBytes());
+                writer.flush();
+            }
+
+            writer.close();
 
         }catch (Exception e){
             e.printStackTrace();
