51d1b4d17b9b0db83945aa6c05dd5c1f629a778b
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-11 23:48:49 +0300

    assets folder and json file added

---
+++ app/src/main/java/Controllers/FileWriterController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import java.util.List;
+import Model.Dustbin;
+
+/**
+ * Created by ILIAS on 11/7/2018.
+ */
+
+public class FileWriterController {
+
+
+    public FileWriterController() {
+    }
+
+    public void writeJsonToResources(List<Dustbin> dustbinsList){
+
+
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ app/src/main/java/DAO/FileWriter.java
@@ -0,0 +1,8 @@
+package DAO;
+
+/**
+ * Created by ILIAS on 11/7/2018.
+ */
+
+public class FileWriter {
+}
