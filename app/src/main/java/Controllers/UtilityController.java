456a1ae8d322b025f0d2f5774589a60f3620948b
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-12 22:21:28 +0300

    added utility controller class

---
+++ app/src/main/java/Controllers/UtilityController.java
@@ -0,0 +1,30 @@
+package Controllers;
+
+import Model.Coordinates;
+import Model.Dustbin;
+import Model.Id;
+
+/**
+ * Created by ILIAS on 12/7/2018.
+ */
+
+public class UtilityController {
+
+    private Id _id;
+    private Coordinates coordinates;
+    private Dustbin dustbin;
+
+    public UtilityController() {
+    }
+
+
+    public Dustbin createDustbin(String dustbinLocation, String dustbinLatitude, String dustbinLongitude){
+
+        Coordinates _coordinates = new Coordinates(Double.parseDouble(dustbinLatitude),Double.parseDouble(dustbinLongitude));
+        Dustbin dustbin = new Dustbin(dustbinLocation,_coordinates);
+
+        return dustbin;
+
+    }
+
+}
