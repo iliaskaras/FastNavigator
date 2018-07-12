d6f284d391bf73d523ccfeb85f41100bfe17f1fa
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-12 22:20:18 +0300

    added methods to read files from raw and from app's data folder

--- app/src/main/java/Controllers/FileReaderController.java
+++ app/src/main/java/Controllers/FileReaderController.java
@@ -3,17 +3,15 @@ package Controllers;
 import android.content.Context;
 import android.os.Environment;
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 
-import Model.Dustbin;
+import gr.onetouchaway.findeverything.fast_navigator.R;
 
 /**
  * Created by ILIAS on 11/7/2018.
@@ -22,8 +20,7 @@ import Model.Dustbin;
 public class FileReaderController {
     private Context mContext;
     final static String fileName = "data.txt";
-    final static String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-//            + "/instinctcoder/readwrite/" ;
+    final static String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Controllers/";
     final static String TAG = FileReaderController.class.getName();
 
     public FileReaderController() {
@@ -33,7 +30,7 @@ public class FileReaderController {
         this.mContext = mContext;
     }
 
-    public String readTxtToResources(){
+    public String readTxtFile(String fileName){
         String line = null;
 
         try {
@@ -57,6 +54,28 @@ public class FileReaderController {
         catch(IOException ex) {
             Log.d(TAG, ex.getMessage());
         }
+        return line;
+    }
+
+    public String readUserDistancesFile() {
+        String line = null;
+
+        InputStream inputStream = this.mContext.getResources().openRawResource(R.raw.user_distances);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            while ( (line = bufferedReader.readLine()) != null )
+            {
+                stringBuilder.append(line + System.getProperty("line.separator"));
+            }
+
+            line = stringBuilder.toString();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return line;
     }
 
