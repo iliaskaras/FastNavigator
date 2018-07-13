4f247e255d6bae6289e61a8e8925ba2558d28e58
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-13 17:33:46 +0300

    changes to code for using our newly added controllers

--- app/src/main/java/Controllers/UtilityControllers/UtilityController.java
+++ app/src/main/java/Controllers/UtilityControllers/UtilityController.java
@@ -1,8 +1,10 @@
 package Controllers.UtilityControllers;
 
+import Exceptions.MyCoordinateException;
+import Exceptions.MyDustbinException;
 import Model.Coordinates;
 import Model.Dustbin;
-import Model.Id;
+//import Model.Id;
 
 /**
  * Created by ILIAS on 12/7/2018.
@@ -10,20 +12,20 @@ import Model.Id;
 
 public class UtilityController {
 
-    private Id _id;
-    private Coordinates coordinates;
-    private Dustbin dustbin;
 
     public UtilityController() {
     }
 
 
-    public Dustbin createDustbin(String dustbinLocation, String dustbinLatitude, String dustbinLongitude){
+    public Dustbin createDustbin(String dustbinLocation, String dustbinLatitude, String dustbinLongitude) throws MyCoordinateException, MyDustbinException {
+        CoordinateUtilController coordinateUtilController = new CoordinateUtilController(dustbinLatitude,dustbinLongitude);
+//        Coordinates _coordinates = new Coordinates(Double.parseDouble(dustbinLatitude),Double.parseDouble(dustbinLongitude));
+        Coordinates _coordinates = coordinateUtilController.getCoordinateObject();
+        DustbinUtilController dustbinUtilController = new DustbinUtilController(dustbinLocation,_coordinates);
+        Dustbin _dustbin = dustbinUtilController.getDustbinObject();
+//        Dustbin dustbin = new Dustbin(dustbinLocation,_coordinates);
 
-        Coordinates _coordinates = new Coordinates(Double.parseDouble(dustbinLatitude),Double.parseDouble(dustbinLongitude));
-        Dustbin dustbin = new Dustbin(dustbinLocation,_coordinates);
-
-        return dustbin;
+        return _dustbin;
 
     }
 
