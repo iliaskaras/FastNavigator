b8a2ac3d2448e406a8c83904ce1713b3d9808b56
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-15 23:33:40 +0300

    removed comments

--- app/src/main/java/Model/Dustbin.java
+++ app/src/main/java/Model/Dustbin.java
@@ -1,31 +1,21 @@
 package Model;
 
-import com.google.gson.annotations.SerializedName;
-
 /**
  * Created by ILIAS on 3/7/2018.
  */
 
 public class Dustbin {
 
-//    @SerializedName("$location")
     private String location;
-
-//    @SerializedName("$oid")
     private Id _id;
-
-//    @SerializedName("$coordinates")
     private Coordinates coordinates = new Coordinates();
 
-    public Dustbin(String location, Coordinates coordinates,  Id _id) {
+    public Dustbin() {
+    }
+
+    public Dustbin(String location, Coordinates coordinates) {
         this.location = location;
         this.coordinates = coordinates;
-        this._id = _id;
-        try{
-
-        }catch(Exception ex){
-            System.out.println("exception caught");
-        }
     }
 
     public Id get_id() {
