fd0e8a1f29ca75aad3b92cc10984abfa9a010855
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-16 23:39:17 +0300

    made it to implement Serializable class

--- app/src/main/java/Model/Id.java
+++ app/src/main/java/Model/Id.java
@@ -2,11 +2,13 @@ package Model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
+
 /**
  * Created by ILIAS on 4/7/2018.
  */
 
-public class Id {
+public class Id implements Serializable{
     @SerializedName("$oid")
     private String oid;
 
@@ -15,6 +17,13 @@ public class Id {
     }
 
     public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public Id() {
+    }
+
+    public Id(String oid) {
         this.oid = oid;
     }
 
