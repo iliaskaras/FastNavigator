dc37d45686a48b7929f37b0223fd50b3bf626cde
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-16 22:57:47 +0300

    removed comments

--- app/src/main/java/Controllers/UtilityControllers/UtilityController.java
+++ app/src/main/java/Controllers/UtilityControllers/UtilityController.java
@@ -4,7 +4,6 @@ import Exceptions.MyCoordinateException;
 import Exceptions.MyDustbinException;
 import Model.Coordinates;
 import Model.Dustbin;
-//import Model.Id;
 
 /**
  * Created by ILIAS on 12/7/2018.
@@ -19,11 +18,9 @@ public class UtilityController {
 
     public Dustbin createDustbin(String dustbinLocation, String dustbinLatitude, String dustbinLongitude) throws MyCoordinateException, MyDustbinException {
         CoordinateUtilController coordinateUtilController = new CoordinateUtilController(dustbinLatitude,dustbinLongitude);
-//        Coordinates _coordinates = new Coordinates(Double.parseDouble(dustbinLatitude),Double.parseDouble(dustbinLongitude));
         Coordinates _coordinates = coordinateUtilController.getCoordinateObject();
         DustbinUtilController dustbinUtilController = new DustbinUtilController(dustbinLocation,_coordinates);
         Dustbin _dustbin = dustbinUtilController.getDustbinObject();
-//        Dustbin dustbin = new Dustbin(dustbinLocation,_coordinates);
 
         return _dustbin;
 
