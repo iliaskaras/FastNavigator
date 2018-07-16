4759ccdf448967918c8c48bbddd2c97a79d12802
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-16 23:40:44 +0300

    implements Parcelable, added CREATOR and made it parcelable

--- app/src/main/java/Model/Dustbin.java
+++ app/src/main/java/Model/Dustbin.java
@@ -1,10 +1,13 @@
 package Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by ILIAS on 3/7/2018.
  */
 
-public class Dustbin {
+public class Dustbin implements Parcelable {
 
     private String location;
     private Id _id;
@@ -17,6 +20,24 @@ public class Dustbin {
         this.location = location;
         this.coordinates = coordinates;
     }
+
+    protected Dustbin(Parcel in) {
+        location = in.readString();
+        coordinates = (Coordinates) in.readSerializable();
+        _id = (Id) in.readSerializable();
+    }
+
+    public static final Creator<Dustbin> CREATOR = new Creator<Dustbin>() {
+        @Override
+        public Dustbin createFromParcel(Parcel in) {
+            return new Dustbin(in);
+        }
+
+        @Override
+        public Dustbin[] newArray(int size) {
+            return new Dustbin[size];
+        }
+    };
 
     public Id get_id() {
         return _id;
@@ -40,5 +61,18 @@ public class Dustbin {
 
     public void setCoordinates(Coordinates coordinates) {
         this.coordinates = coordinates;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getLocation());
+        dest.writeSerializable(getCoordinates());
+        dest.writeSerializable(get_id());
+
     }
 }
