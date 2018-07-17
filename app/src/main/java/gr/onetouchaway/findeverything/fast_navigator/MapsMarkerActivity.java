4b4c8931eb22a63eecdcca63244acd24a6d57182
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-17 22:22:31 +0300

    added onMarkerClick event, and drawing of directions + reset of map

--- app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
+++ app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
@@ -1,11 +1,22 @@
 package gr.onetouchaway.findeverything.fast_navigator;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 import Controllers.GoogleMapController;
@@ -17,10 +28,12 @@ import Model.Dustbin;
  */
 
 public class MapsMarkerActivity extends AppCompatActivity
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
 
     private List<Dustbin> dustbins = new ArrayList<Dustbin>();
-
+    private List<LatLng> latLngList;
+    private GoogleMap mMap;
+    final static String TAG = MapsMarkerActivity.class.getName();
     // Include the OnCreate() method here too, as described above.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,9 @@ public class MapsMarkerActivity extends AppCompatActivity
 
         initializeDustbinList();
         initializeLatLngMarks();
+
+        latLngList = new ArrayList<LatLng>();
+
 
     }
 
@@ -51,12 +67,84 @@ public class MapsMarkerActivity extends AppCompatActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
         GoogleMapController googleMapController = new GoogleMapController();
-        googleMapController.addMarkersToGoogleMap(null, null);
-
         googleMapController.addMarkersToGoogleMap(googleMap, this.dustbins);
         googleMapController.zoomCameraMap(googleMap, 10.0f);
+
+        mMap = googleMap;
+//        mMap.setOnMapClickListener(this);
+        mMap.setOnMarkerClickListener(this);
     }
 
 
+    @Override
+    public void onMapClick(LatLng latLng) {
+//        if(latLngList.size() > 2){
+//            refreshMap(mMap);
+//            latLngList.clear();
+//        }
+//
+//        latLngList.add(latLng);
+//        Log.d(TAG, "Marker number " + latLngList.size());
+//
+//        MarkerOptions options = new MarkerOptions();
+//        options.position(this.dustbins.get(0).getLatLng());
+//        options.title(this.dustbins.get(0).getLocation());
+//        mMap.addMarker(options);
+//        mMap.addMarker(new MarkerOptions().position(latLng));
+//        LatLng defaultLocation = options.getPosition();
+//        LatLng destinationLocation = latLng;
+//        //use Google Direction API to get the route between these Locations
+////        String directionApiPath = Helper.getUrl(String.valueOf(defaultLocation.latitude), String.valueOf(defaultLocation.longitude),
+////                String.valueOf(destinationLocation.latitude), String.valueOf(destinationLocation.longitude));
+////        Log.d(TAG, "Path " + directionApiPath);
+//
+//        if(latLngList.size() > 1){
+//            drawRouteOnMap(mMap,latLngList);
+//
+//        }
+//        drawRouteOnMap(mMap,latLngList);
+    }
 
+    private void drawRouteOnMap(GoogleMap map, List<LatLng> positions){
+        PolylineOptions options = new PolylineOptions().width(5).color(Color.BLUE).geodesic(true);
+        options.addAll(positions);
+        Polyline polyline = map.addPolyline(options);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(positions.get(1).latitude, positions.get(1).longitude))
+                .zoom(11)
+                .build();
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+
+    private void refreshMap(GoogleMap mapInstance){
+        mapInstance.clear();
+    }
+
+    private void resetMap(GoogleMap mapInstance){
+        GoogleMapController googleMapController = new GoogleMapController();
+        googleMapController.addMarkersToGoogleMap(mapInstance, this.dustbins);
+        googleMapController.zoomCameraMap(mapInstance, 10.0f);
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        Log.d(TAG, "MARKER " + marker.getTitle());
+
+        if(latLngList.size() > 2){
+            refreshMap(mMap);
+            resetMap(mMap);
+            latLngList.clear();
+        }
+
+        latLngList.add(marker.getPosition());
+
+        if(latLngList.size() > 1){
+            drawRouteOnMap(mMap,latLngList);
+        }
+
+        return false;
+    }
 }
