952694f80d50516bf7c325da7c42037335dc1c4e
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-17 22:39:52 +0300

    mock method to get shortest distance

--- app/src/main/java/Controllers/DijskstraController.java
+++ app/src/main/java/Controllers/DijskstraController.java
@@ -1,8 +1,30 @@
 package Controllers;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.List;
+
+import Model.Dustbin;
+
 /**
  * Created by ILIAS on 10/7/2018.
  */
 
 public class DijskstraController {
+
+
+
+    public DijskstraController() {
+    }
+
+    public static LatLng findShortestPath(List<Dustbin> dustbinList, Marker currentDustbinLocation){
+
+        String location = currentDustbinLocation.getTitle();
+        LatLng latLng = currentDustbinLocation.getPosition();
+
+        return latLng;
+
+    }
+
 }
