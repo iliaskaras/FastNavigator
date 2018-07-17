e10bed4f1d76bb6d85c096b140851c1d138347d5
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-17 21:12:28 +0300

    added methods to create the markers on the map, based on our dustbin list

--- app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
+++ app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
@@ -1,19 +1,15 @@
 package gr.onetouchaway.findeverything.fast_navigator;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import java.util.ArrayList;
 import java.util.List;
-
+import Controllers.GoogleMapController;
+import Controllers.UtilityControllers.DustbinUtilController;
 import Model.Dustbin;
 
 /**
@@ -38,6 +34,8 @@ public class MapsMarkerActivity extends AppCompatActivity
         mapFragment.getMapAsync(this);
 
         initializeDustbinList();
+        initializeLatLngMarks();
+
     }
 
     private void initializeDustbinList(){
@@ -45,13 +43,20 @@ public class MapsMarkerActivity extends AppCompatActivity
         this.dustbins = (List<Dustbin>) i.getSerializableExtra("dustbins");
     }
 
+    private void initializeLatLngMarks(){
+        DustbinUtilController dustbinUtilController = new DustbinUtilController();
+        this.dustbins = dustbinUtilController.initializeLatLngMarks(this.dustbins);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        GoogleMapController googleMapController = new GoogleMapController();
+        googleMapController.addMarkersToGoogleMap(null, null);
+
+        googleMapController.addMarkersToGoogleMap(googleMap, this.dustbins);
+        googleMapController.zoomCameraMap(googleMap, 10.0f);
     }
+
+
+
 }
