6be22c015316bd15bc17fde6a29cefd9f7466b5c
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-18 23:34:40 +0300

    added a simple mechanism to find shortest Distance object based on SpericalUtil class

--- app/src/main/java/Controllers/DijkstraController.java
+++ app/src/main/java/Controllers/DijkstraController.java
@@ -2,9 +2,14 @@ package Controllers;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.maps.android.SphericalUtil;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import Model.CustomDistanceComparator;
+import Model.Distance;
 import Model.Dustbin;
 
 /**
@@ -23,10 +28,29 @@ public class DijkstraController {
         String location = currentDustbinLocation.getTitle();
         LatLng latLng = currentDustbinLocation.getPosition();
 
-        return latLng;
+        Distance foundShortestDistance = dijkstraAlgorithm(dustbinList,latLng);
+
+        return foundShortestDistance.getLocation();
 
     }
 
-    private void
+    private static Distance dijkstraAlgorithm(List<Dustbin> dustbinList, LatLng currentDustbinLocation){
+
+        List<Distance> distances = new ArrayList<>();
+
+        LatLng startLatLng = new LatLng(currentDustbinLocation.latitude, currentDustbinLocation.longitude);
+
+        for(Dustbin dustbin : dustbinList){
+            LatLng endLatLng = new LatLng(dustbin.getLatLng().latitude, dustbin.getLatLng().longitude);
+            double distance = SphericalUtil.computeDistanceBetween(startLatLng, endLatLng);
+            Distance distanceObject = new Distance(distance,dustbin.getLocation(),dustbin.getLatLng());
+            distances.add(distanceObject);
+        }
+
+        Collections.sort(distances, new CustomDistanceComparator());
+
+        /** remove the first from the list because it has distance 0 meaning it compares to itself */
+        return distances.get(1);
+    }
 
 }
