d2d720bd152a47abae5f4ca647b1df6c5238812b
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-19 22:54:54 +0300

    added some exception handling

--- app/src/main/java/Controllers/UtilityControllers/DistanceUtilController.java
+++ app/src/main/java/Controllers/UtilityControllers/DistanceUtilController.java
@@ -1,7 +1,13 @@
 package Controllers.UtilityControllers;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
 
+import Exceptions.ErrorCodes.ExceptionErrorCodes;
+import Exceptions.MyCoordinateException;
+import Exceptions.MyDistanceException;
+import Exceptions.MyDustbinException;
 import Model.Distance;
 
 /**
@@ -23,12 +29,36 @@ public class DistanceUtilController {
         this.location = location;
     }
 
-    public Distance getDistanceObject(double distance, String destLocation, LatLng location){
-
+    public Distance getDistanceObject(double distance, String destLocation, LatLng location) throws MyDistanceException {
+        try{
+            locationValidation(destLocation);
+        } catch (MyDistanceException ex){
+            Log.d(TAG, ex.getMessage());
+            throw new MyDistanceException(ExceptionErrorCodes.WRONG_LOCATION);
+        }
         Distance distanceObject = new Distance(distance,destLocation,location);
 
         return distanceObject;
     }
+
+    private boolean locationValidation(String location) throws MyDistanceException{
+        boolean isLocationEntered = false;
+
+        try{
+
+            if (!location.isEmpty()){
+                isLocationEntered = true;
+            }
+
+        } catch(NullPointerException ex){
+            Log.d(TAG, ex.getMessage());
+            throw new MyDistanceException(ex, ExceptionErrorCodes.WRONG_LOCATION);
+        }
+
+        return isLocationEntered;
+    }
+
+
 
 
 }
