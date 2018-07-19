2fef0579d548b8cedf35ded7b03fc15511453191
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-19 22:44:13 +0300

    fixed push error - re upload of the util class

--- app/src/main/java/Controllers/UtilityControllers/DistanceUtilController.java
+++ app/src/main/java/Controllers/UtilityControllers/DistanceUtilController.java
@@ -1,11 +1,34 @@
 package Controllers.UtilityControllers;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import Model.Distance;
+
 /**
  * Created by ILIAS on 19/7/2018.
  */
 
 public class DistanceUtilController {
     final static String TAG = DistanceUtilController.class.getName();
-    private String
+    private double distance;
+    private String destLocation;
+    private LatLng location;
+
+    public DistanceUtilController() {
+    }
+
+    public DistanceUtilController(double distance, String destLocation, LatLng location) {
+        this.distance = distance;
+        this.destLocation = destLocation;
+        this.location = location;
+    }
+
+    public Distance getDistanceObject(double distance, String destLocation, LatLng location){
+
+        Distance distanceObject = new Distance(distance,destLocation,location);
+
+        return distanceObject;
+    }
+
 
 }
