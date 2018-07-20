f0663d8ce0797697671bcc3126070d060cdfdde0
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-20 19:41:39 +0300

    added async tasks to get directions between two points on map and draw them

--- app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
+++ app/src/main/java/gr/onetouchaway/findeverything/fast_navigator/MapsMarkerActivity.java
@@ -6,17 +6,34 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import Controllers.DirectionsParser;
 import Controllers.ShortestDistanceController;
 import Controllers.GoogleMapController;
 import Controllers.UtilityControllers.DustbinUtilController;
@@ -35,6 +52,7 @@ public class MapsMarkerActivity extends AppCompatActivity
     private GoogleMap mMap;
     private Marker yourLocationMarker;
     private boolean clickYourLocationClicked = false;
+    private boolean locationIsChosed = false;
     private Button btnClickYourLocation;
     private GoogleMapController googleMapController;
 
@@ -78,9 +96,150 @@ public class MapsMarkerActivity extends AppCompatActivity
     }
 
     private void findShortestPath() throws MyDistanceException {
-        LatLng foundShortestPath = ShortestDistanceController.findShortestPath(dustbins, yourLocationMarker);
+        LatLng foundShortestDest = ShortestDistanceController.findShortestPath(dustbins, yourLocationMarker);
 
-        googleMapController.manuallyDrawDirection(yourLocationMarker.getPosition(),foundShortestPath,mMap,dustbins);
+//        googleMapController.manuallyDrawDirection(yourLocationMarker.getPosition(),foundShortestDest,mMap,dustbins);
+
+
+        String url = getRequestURL(foundShortestDest, yourLocationMarker.getPosition());
+        TaskRequestDirections taskRequestDirections = new TaskRequestDirections();
+        taskRequestDirections.execute(url);
+    }
+
+    private String getRequestURL(LatLng destination, LatLng origin ){
+
+        //value of origin
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        //value of dest
+        String str_destination = "destination=" + destination.latitude + "," + destination.longitude;
+        //set value enable the sensor
+        String sensor = "sensor=false";
+        //mode for finding direction
+        String mode = "mode=driving";
+        //building of the full param
+        String param = str_origin + "&" + str_destination + "&" + sensor + "&" + mode;
+        //output format
+        String output = "json";
+
+        //create url to request "https://maps.google.googleapis.com/maps/api/directions/"
+        String url = "https://maps.google.googleapis.com/maps/api/directions/" + output + "?" + param;
+
+        return url;
+    }
+
+    public class TaskRequestDirections extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String responseString = "";
+            try{
+                responseString = requestDirection(params[0]);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //Parse json here
+            TaskParser taskParser = new TaskParser();
+            taskParser.execute(s);
+        }
+    }
+
+    public class TaskParser extends AsyncTask<String, Void,  List<List<HashMap<String, String>>>>{
+
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... params) {
+            JSONObject jsonObject = null;
+            List<List<HashMap<String, String>>> routes = null;
+            try {
+                jsonObject = new JSONObject(params[0]);
+                DirectionsParser directionsParser = new DirectionsParser();
+                routes = directionsParser.parse(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> lists) {
+            //Get list of routes and display them on map
+
+            ArrayList points = null;
+            PolylineOptions polylineOptions = null;
+
+            for(List<HashMap<String, String>> path : lists){
+                points = new ArrayList();
+                polylineOptions = new PolylineOptions();
+
+                for(HashMap<String, String> point : path){
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lon = Double.parseDouble(point.get("lon"));
+
+                }
+
+                polylineOptions.addAll(points);
+                polylineOptions.width(15);
+                polylineOptions.color(Color.BLUE);
+                polylineOptions.geodesic(true);
+
+            }
+
+            if(polylineOptions!=null){
+                mMap.addPolyline(polylineOptions);
+            } else {
+                Toast.makeText(getApplicationContext(),"direction not found!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String requestDirection(String reqUrl) throws IOException {
+
+        String responseString = "";
+        InputStream inputStream = null;
+        HttpURLConnection httpURLConnection = null;
+        try{
+            URL url = new URL(reqUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+
+            //Get the response request
+            inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while((line = bufferedReader.readLine())!= null){
+                stringBuffer.append(line);
+            }
+
+            responseString = stringBuffer.toString();
+            bufferedReader.close();
+            inputStreamReader.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            if(inputStream != null){
+                inputStream.close();
+            }
+            if(httpURLConnection!=null){
+                httpURLConnection.disconnect();
+            }
+        }
+
+        return responseString;
     }
 
     @Override
@@ -98,6 +257,7 @@ public class MapsMarkerActivity extends AppCompatActivity
 
         if(yourLocationMarker != null){
             try{
+                locationIsChosed = true;
                 yourLocationMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
             } catch (Exception ex){
                 Log.d(TAG+"onMarkerClick exception", ex.getMessage());
@@ -120,6 +280,7 @@ public class MapsMarkerActivity extends AppCompatActivity
             btnClickYourLocation.setText("Choose your location in map");
 
         } else {
+            locationIsChosed = false;
             clickYourLocationClicked = false;
             btnClickYourLocation.setClickable(true);
             btnClickYourLocation.setPressed(false);
