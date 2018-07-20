cb4ed29b7aac404a3f7947bacad44f3158cd61f3
Karatsin Ilias <hlias.karas@yahoo.gr>
2018-07-20 19:43:25 +0300

    added proper closure of resources at finally section of try catch

--- app/src/main/java/Services/PutService.java
+++ app/src/main/java/Services/PutService.java
@@ -19,31 +19,37 @@ public class PutService {
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void PutHTTPData(String urlString, String newValue){
+        HttpURLConnection httpURLConnection = null;
+
         try{
             URL url = new URL(urlString);
-            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection = (HttpURLConnection)url.openConnection();
 
-            urlConnection.setRequestMethod("PUT");
-            urlConnection.setDoOutput(true);
+            httpURLConnection.setRequestMethod("PUT");
+            httpURLConnection.setDoOutput(true);
 
             byte[] out = newValue.getBytes(StandardCharsets.UTF_8);
             int length = out.length;
 
-            urlConnection.setFixedLengthStreamingMode(length);
-            urlConnection.setRequestProperty("Content-Type","application/json; charset-UTF-8");
-            urlConnection.connect();
+            httpURLConnection.setFixedLengthStreamingMode(length);
+            httpURLConnection.setRequestProperty("Content-Type","application/json; charset-UTF-8");
+            httpURLConnection.connect();
 
-            try(OutputStream os = urlConnection.getOutputStream()){
+            try(OutputStream os = httpURLConnection.getOutputStream()){
                 os.write(out);
 
             }
 
-            InputStream response = urlConnection.getInputStream();
+            InputStream response = httpURLConnection.getInputStream();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if(httpURLConnection!=null){
+                httpURLConnection.disconnect();
+            }
         }
 
     }
